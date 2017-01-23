54aa58abe6028dd0be4d42db9a0e12019f114b60
Chloe Duan <Chloe Duan>
2017-01-23 15:01:13 -0500

    get_imgs()

--- A1.py
+++ A1.py
@@ -1,4 +1,3 @@
-# import PySide
 from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,75 +11,19 @@ import os
 from scipy.ndimage import filters
 import urllib
 
-print("okay got here")
+def get_imgs(read_folder):
+    imgs = np.empty([0, 1024])
+    filename_to_img = []
+    for filename in os.listdir(read_folder):
+        try:
+            img = imread(os.path.join(read_folder,filename), flatten=True)
+            if img is not None:
+                imgs = vstack((imgs, reshape(np.ndarray.flatten(img), [1, 1024])))
+                filename_to_img.append(filename) 
 
-act = list(set([a.split("\t")[0] for a in open("facescrub_actors.txt").readlines()]))
+        except Exception as e:
+            print(filename, str(e))
+    return imgs, filename_to_img
 
-
-def timeout(func, args=(), kwargs={}, timeout_duration=1, default=None):
-    '''From:
-    http://code.activestate.com/recipes/473878-timeout-function-using-threading/'''
-    import threading
-    class InterruptableThread(threading.Thread):
-        def __init__(self):
-            threading.Thread.__init__(self)
-            self.result = None
-
-        def run(self):
-            try:
-                self.result = func(*args, **kwargs)
-            except:
-                self.result = default
-
-    it = InterruptableThread()
-    it.start()
-    it.join(timeout_duration)
-    if it.isAlive():
-        return False
-    else:
-        return it.result
-        
-        
-def rgb2gray(rgb):
-    '''Return the grayscale version of the RGB image rgb as a 2D numpy array
-    whose range is 0..1
-    Arguments:
-    rgb -- an RGB image, represented as a numpy array of size n x m x 3. The
-    range of the values is 0..255
-    '''
-    
-    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
-    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-
-    return gray/255.
-
-testfile = urllib.URLopener()            
-
-
-#Note: you need to create the uncropped folder first in order 
-#for this to work
-
-for a in act:
-    name = a.split()[1].lower()
-    i = 0
-    for line in open("facescrub_actors.txt"):
-        if a in line:
-            crop_coords = line.split()[5].split(",")
-            crop_coords = tuple(map(int, crop_coords))
-            filename = name+str(i)+"-"+str(crop_coords)+'.'+line.split()[4].split('.')[-1]
-            #A version without timeout (uncomment in case you need to 
-            #unsupress exceptions, which timeout() does)
-            try:
-                testfile.retrieve(line.split()[4], "uncropped/"+filename)
-            except Exception as e:
-                print(str(e))
-            #timeout is used to stop downloading images which take too long to download
-            #timeout(testfile.retrieve, (line.split()[4], "uncropped/"+filename), {}, 30)
-            if not os.path.isfile("uncropped/"+filename):
-                continue
-
-            
-            print filename
-            i += 1
-    
-    
+imgs, filename_to_img = get_imgs("filtered_male/")
+theta = ones([1, 1024])
